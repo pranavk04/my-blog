8974e88294d5acc2d3249e82f60043ad907d3fbe
Pranav Konda <pranav@Pranavs-MacBook-Pro.local>
2021-09-11 00:35:25 -0400

    styling navbar, latex support again, color palette finalized, margins set

--- components/navbar.js
+++ components/navbar.js
@@ -1,25 +1,47 @@
 import Link from 'next/link'
+import React, { Component, useState } from 'react'
+
+class NavLink extends Component {
+	render() {
+
+		const { onPress, where, text } = this.props;
+
+		return (	
+			<Link href={where}>
+				<a onClick={onPress} className="text-lg block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4"><h1>{text}</h1></a>
+			</Link>
+		)
+	}
+}
 
 function Navbar() {
+
+	const [expanded, setExpanded] = useState(true);
+
 	return (
 		<div>
-			<div>
+			<div className="flex items-center justify-between flex-wrap p-10">
+
+				<div className="flex items-center flex-shrink-0 text-white mr-10">
 					<Link href='/'>
-						<a><h1>Home</h1></a>
+						<span className="font-semibold text-2xl tracking-tight">Pranav</span>
 					</Link>
+				</div>
 
-					<Link href='/blog'>
-						<a><h1>Blog</h1></a>
-					</Link>
+				<div className="block lg:hidden">
+					<button onClick={() => {setExpanded(!expanded); console.log(expanded);}} class="flex items-center px-3 py-2 text-black-200">
+						      <svg className="fill-current h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+					</button>
+				</div>
 
-
-					<Link href='/about-me'>
-						<a><h1>About Me</h1></a>
-					</Link>
-
-					<Link href='/resources'>
-						<a ><h1>Resources</h1></a>
-					</Link>
+				<div className={"w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto" + (expanded ? " flex" : " hidden")}>
+					<div className="text-sm lg:flex-grow">
+						<NavLink where='/' text='Home' onPress={() => setExpanded(false)}/>
+						<NavLink where='/blog' text='Blog' onPress={() => setExpanded(false)}/>
+						<NavLink where='/about-me' text='About Me' onPress={() => setExpanded(false)}/>
+						<NavLink where='/resources' text='Resources' onPress={() => setExpanded(false)}/>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
 function Home() {
 	return (
-		<div>
-			<span> 
-				<h1>Hi</h1>
-			</span>
-
-			<div>
-				<p>This is a test of my intelligence.</p>
-				<p>But this is also a scam?</p>				
-				<p> We all know that Thonkulus never spoke again after his wife exposed him for having the smallest thonker in the world...</p>
-			</div>
+		<div className="mr-10 ml-10">
+			Lorem ipsum s
 		</div>
 	);
 }
