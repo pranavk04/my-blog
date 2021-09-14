131ae1f94fd1a9fb0e8859b3038bfe8d9eec62d2
Pranav Konda <pranav@Pranavs-MBP.attlocal.net>
2021-09-14 00:48:31 -0400

    absolute bruh moment

--- pages/lectures.js
+++ pages/lectures.js
@@ -2,7 +2,7 @@ export default function Lectures() {
 	return (
 		<div>
 			<h1>Lectures</h1>
-			<p>Here are some lectures, now go watch all of them<p>
+			<p>Here are some lectures, now go watch all of them</p>
 		</div>
 		)
 }
