f03bf2ffade9e88e292f662dddb0dc0fb775bb82
pranavk04 <theotaku1996@gmail.com>
2022-05-16 07:22:50 -0400

    edited text on nature page

--- pages/nature.js
+++ pages/nature.js
@@ -9,7 +9,7 @@ export default function nature({ data }) {
     return (
         <div className='space-y-4'>
             <h1 className="text-center text-primary-900 mb-4 font-semibold text-3xl md:text-5xl tracking-tight">Nature</h1>
-            <p>Below you'll find pictures I take of my garden or things I find in nature. They're grouped roughly by what they are, although the landscapes category is just pictures of scenery. Click view to expand the box and view the pictures!</p>
+        <p>Below you'll find pictures I take of my garden or things I find in nature. They're grouped roughly by what they are, although the landscapes category is just pictures of scenery. Click view to well, view them! </p>
             <div className='flex items-center justify-center'>
                 <div className='grid grid-cols-1 md:grid-cols-2 md:gap-8 gap-4'>
                     {data.map((value) => (
