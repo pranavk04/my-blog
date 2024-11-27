e70b4fa640ee75a05039a7c78a5d0ee4b272d784
pranav <theotaku1996@gmail.com>
2024-11-27 00:53:07 -0500

    more kritis

---
+++ src/routes/compositions/hiranyarathavahana/+page.svelte
@@ -0,0 +1,93 @@
+<div class='space-y-4'>
+	<h1 class="md:text-3xl font-shobhika text-3xl font-black text-center">Hiraṇyarathavāhana</h1>
+	<p class="text-gray-700 text-center"> rāga: śaṅkarābharaṇa, tāḷa: ādi.</p>
+
+    <div class="text-center" style="font-family: Shobhika">
+        <h3>पल्लवि</h3>
+        <p>
+            हिरण्यरथवाहन पावकचक्षसा 
+        </p>
+        <p>
+            देवः सविता मां पाहि मां पाहि
+        </p>
+        <h3>अनुपल्लवि</h3>
+
+        <p>
+            आरोग्यदातार्यविनुत 
+        </p>
+        <p>
+            विभ्राजमान विश्वद्रष्टः   
+        </p>
+
+        <h3>चरणम्</h3>
+        <p>
+            वाजसनेयवेदप्रकाशिकः   
+        </p>
+        <p>
+            प्रतिदिन<b>वेङ्कटप्रणव</b>नुत  
+        </p>
+        <p>
+            श्री सूर्यनारायण परब्रह्मण
+        </p>
+    </div>
+
+    <div class="text-center">
+        <h3>pallavi</h3>
+        <p>
+            hiraṇyarathavāhana pāvakacakṣasā 
+        </p>
+        <p>
+            devaḥ savitā māṁ pāhi māṁ pāhi  
+        </p>
+        <h3>anupallavi</h3>
+        <p>
+            ārogyadātāryavinuta 
+        </p>
+        <p>
+            vibhrājamāna viśvadraṣṭāḥ
+        </p>
+        <h3>caraṇam</h3>
+        <p>
+            vājasaneyavedaprakāśikaḥ  
+        </p>
+        <p>
+            pratidina<b>veṅkaṭapraṇava</b>nuta
+        </p>
+        <p>
+            śrī sūryanārāyaṇa parabrahmaṇa 
+        </p>
+    </div>
+
+    <div class="space-y-4">
+        <p>Inspired by the weighty caukakāla kr̥ti-s (slow tempo, in this case I am referencing śrī rājagopālabāla in sāveri and bālagopala pālayāṣumām in bhairavi) masterpieces of Muddusvāmi Dīkṣitulu, comes my own caukakāla kr̥ti in the immortal śaṅkarābharaṇa rāga, considered by the pūrvācārya-s to be the shining example of the perfect rāga. All of the trinity have composed stellar, beautiful compositions in the rāga, such as Tyāgarāju's manasu svādhīnamaina and svararāgasudhā, Śyāma Śāstrulu's sarojadaḷanetri and devimīnanetri, and the kr̥ti-s śrī dakṣiṇāmūrte, akṣayaliṅgavibho, and śrī kamalāmbā of Dīkṣitulu, among many many others. 
+        </p>
+
+        <p>After my morning saṅgītābhyāsa from my Guru I was walking to the library to study under the rays of the morning sun when this kr̥ti came to me, inspired by the warmth of the sunlight contrasting with the morning breeze. Here's a rough translation of the kr̥ti: </p>
+
+        <div class="text-center">
+            <p>O one who rides the golden chariot, with your purifying gaze,</p>
+            <p>protect me, Savitr̥, the shining one!</p>
+            <p>The bestower of health, praised by those who are noble,</p>
+            <p>the one who sees the entire word while glowing!</p>
+            <p>The revealer of the Veda of the Vājasaneyin-s, </p>
+            <p>worshipped every morning by Veṅkaṭapraṇava,</p>
+            <p>Sūryanārayaṇa, the all-encompassing Brahman!</p>
+        </div>
+
+        <p>A particularly apt reader may immediately notice the core inspiration for this kr̥ti coming from the mādhyāhnikārghyapradhāna mantra, quoted here from the śukla yajurveda but appearing in many other śākha-s, which describes Savitr̥ as being borne on a golden chariot (symbolizing the sun rising and coming down [niviś] and drawing away the darkness, kr̥ṣṇā): </p>
+        <div style="font-family: Shobhika" class="text-center space-y-2">
+            <p>आ कृ॒ष्णेन॒ रज॑सा॒ वर्त॑मानो निवे॒शय॑न्न॒मृतं मर्त्य॑ञ्च।</p>
+            <p>हि॒र॒ण्यये॑न सवि॒ता रथे॒ना दे॒वो या॑ति॒ भुव॑नानि॒ पश्य॑न्॥</p>
+        </div>
+        
+        <p>There are other subtle inspirations from the various saura mantra-s of the Veda (which I personally find to contain some of the most beautiful and inspiring imagery amongst the saṁhita-s, rivaled only by the hymns to Devī Uṣas, goddess of the Dawn) and the interested reader as a challenge is recommended to listen to the r̥gvedīya mahāsauram chant and to try and find the references above (or, if you can find it, the vibhrāḷiti anuvāka of the vājasaneya kāṇva saṁhita, my source of the r̥k-s).</p>
+
+        <p>
+            It is difficult to offer a translation of the term parabrahmaṇa, for brahmaṇ is a notoriously challenging concept (as you probably have encountered in any discourse about vedānta philosophy) to render clearly. All-pervasive fundamental essence, perhaps? I'm not going to try to make it seem like I can even make a proper attempt here, as all I have is some vague intuitive grasp at what I think is being implied. In the kāṇva arghyapradhāna vidhi there is the formula "auṁ śrī padminī uṣā saujñā chāyāj sameta śrī sūryanārāyaṇa parabrahmaṇe namaḥ; idamarghyaṁ samarpayāmi," from whence I got the inspiration for the term sūryanārāyaṇaparabrahmaṇa. 
+        </p>
+
+        <p>
+            I enjoy thinking about this kr̥ti for it was inspired by Dīkṣitulu, the Veda saṁhita, and the natural beauty of the Sun's rays in the morning: a wonderful confluence that led to the composition of a wonderful kr̥ti. 
+        </p>
+    </div>
+</div>
